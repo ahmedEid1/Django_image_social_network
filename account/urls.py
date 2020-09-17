6e7554ed52f88569543921ce824fb9810665e0d5
ahmedeid <kingmedo954@gmail.com>
2020-09-17 11:48:08 +0200

    register new user

--- account/urls.py
+++ account/urls.py
@@ -6,30 +6,31 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
-    # path('login', views.user_login, name='login'),
-    path('login', auth_views.LoginView.as_view(), name='login'),
-    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
 
     # password change urls
-    path('password_change', auth_views.PasswordChangeView.as_view(
+    path('password_change/', auth_views.PasswordChangeView.as_view(
         success_url=reverse_lazy('account:password_change_done')), name="password_change"),
 
-    path('password_change/done', auth_views.PasswordChangeDoneView.as_view(),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(),
          name='password_change_done'),
 
     # reset password
-    path('password_reset', auth_views.PasswordResetView.as_view(
+    path('password_reset/', auth_views.PasswordResetView.as_view(
         success_url=reverse_lazy('account:password_reset_done')
         ),
          name='password_reset'),
-    path('password_reset/done', auth_views.PasswordResetDoneView.as_view(),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
          name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
         success_url=reverse_lazy('account:password_reset_complete')
     ),
          name='password_reset_confirm'),
-    path('reset/done', auth_views.PasswordResetCompleteView.as_view(),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
          name='password_reset_complete'),
+
 
 ]
