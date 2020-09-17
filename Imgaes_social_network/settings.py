32a0f0619b6fbcaea68c4b1066b57b376f421da9
ahmedeid <kingmedo954@gmail.com>
2020-09-17 10:51:03 +0200

    resting password if forgotten

--- Imgaes_social_network/settings.py
+++ Imgaes_social_network/settings.py
@@ -125,3 +125,5 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'account:dashboard'
 LOGIN_URL = 'account:login'
 LOGOUT_URL = 'account:logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
