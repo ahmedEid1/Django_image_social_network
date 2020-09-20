e27152bc08e031622a617619f4abad7209581f2a
ahmedeid <kingmedo954@gmail.com>
2020-09-20 16:55:14 +0200

    keeping track of pages views

--- images/views.py
+++ images/views.py
@@ -4,11 +4,18 @@ from django.contrib import messages
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.conf import settings
+import redis
 
 from .forms import ImageCreateForm
 from .models import Image
 from common.decorators import ajax_required
 from actions.utils import create_action
+
+
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
 
 
 @login_required
@@ -35,10 +42,11 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-
+    total_views = r.incr(f'image:{image.id}:views')
     return render(request, 'images/image/details.html',
                   {'section': 'images',
-                   'image': image})
+                   'image': image,
+                   'total_views': total_views})
 
 
 @ajax_required
